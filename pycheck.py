5a5bf54f3882dc757abe25537281024cd6d206d6
Sergey <xxsmotur@gmail.com>
2016-07-23 18:34:14 +0500

    Add floatgen

--- pycheck.py
+++ pycheck.py
@@ -37,3 +37,28 @@ class IntGen(Execution):
 		result = self._gen()
 		print("Total number of tests {0}".format(self.size))
 		print("Passed: {0}".format(result))
+
+
+class FloatGen(Execution):
+	def __init__(self, method, logic, min_value=-1000000, max_value=1000000, size=100):
+		super(Execution, self).__init__()
+		self.method = method
+		self.min_value = min_value
+		self.max_value = max_value
+		self.size = size
+		self.logic = logic
+
+	def _gen(self):
+		count = 0
+		for i in range(self.size):
+			item = numpy.random.rand()
+			point = numpy.random.randint(low=self.min_value, high=self.max_value)
+			if self.exec_method(self.method, point*item, self.logic) is True:
+				count+=1
+		return count
+
+	
+	def gen(self):
+		result = self._gen()
+		print("Total number of tests {0}".format(self.size))
+		print("Passed: {0}".format(result))

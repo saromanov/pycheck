b367d90dcf47585a2749fc18ac09503b8d70ba2d
Sergey <xxsmotur@gmail.com>
2016-07-23 18:43:43 +0500

    Change structure

--- pycheck.py
+++ pycheck.py
@@ -2,9 +2,9 @@ import numpy
 
 class Execution(object):
 	"""Exec provides execution of the test"""
-	def __init__(self, arg):
-		super(Execution, self).__init__()
-		self.arg = arg
+	def __init__(self, size):
+		print(size)
+		self.size = size
 
 	def exec_method(self, method, param, logic):
 		try:
@@ -12,12 +12,17 @@ class Execution(object):
 			return logic(result)
 		except Exception:
 			return False
+
+	def gen(self):
+		result = self._gen()
+		print("Total number of tests {0}".format(self.size))
+		print("Passed: {0}".format(result))
 		
 
 class IntGen(Execution):
 	"""Generation of the random numbers"""
 	def __init__(self, method, logic, min_value=-1000000, max_value=1000000, size=100):
-		super(Execution, self).__init__()
+		super(IntGen, self).__init__(size)
 		self.method = method
 		self.min_value = min_value
 		self.max_value = max_value
@@ -41,11 +46,10 @@ class IntGen(Execution):
 
 class FloatGen(Execution):
 	def __init__(self, method, logic, min_value=-1000000, max_value=1000000, size=100):
-		super(Execution, self).__init__()
+		super(FloatGen, self).__init__(size)
 		self.method = method
 		self.min_value = min_value
 		self.max_value = max_value
-		self.size = size
 		self.logic = logic
 
 	def _gen(self):
@@ -56,9 +60,3 @@ class FloatGen(Execution):
 			if self.exec_method(self.method, point*item, self.logic) is True:
 				count+=1
 		return count
-
-	
-	def gen(self):
-		result = self._gen()
-		print("Total number of tests {0}".format(self.size))
-		print("Passed: {0}".format(result))

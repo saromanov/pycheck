06eb274780f2066d15ec788b817b1532d4933c64
Sergey <xxsmotur@gmail.com>
2016-07-23 17:35:42 +0500

    build without errors

--- pycheck.py
+++ pycheck.py
@@ -3,20 +3,21 @@ import numpy
 class Execution(object):
 	"""Exec provides execution of the test"""
 	def __init__(self, arg):
-		super(Exec, self).__init__()
+		super(Execution, self).__init__()
 		self.arg = arg
 
-	def exec_method(method, param, expected):
+	def exec_method(self, method, param, expected):
 		try:
 			result = method(param)
 			return False if result != expected else True
-		except Exception, e:
+		except Exception:
 			return False
 		
 
-class IntGen(Exception):
+class IntGen(Execution):
 	"""Generation of the random numbers"""
 	def __init__(self, method, min_value=-1000000, max_value=1000000, size=100):
+		super(Execution, self).__init__()
 		self.method = method
 		self.min_value = min_value
 		self.max_value = max_value
@@ -24,9 +25,9 @@ class IntGen(Exception):
 
 	def _gen(self):
 		count = 0
-		for i in range(size):
+		for i in range(self.size):
 			item = numpy.random.randint(low=self.min_value, high=self.max_value)
-			if self.exec_method(method, item) is True:
+			if self.exec_method(self.method, item, 1) is True:
 				count+=1
 		return count
 
